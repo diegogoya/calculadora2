84181b42db788508b9716cc6f9dd5781b5cb1b7c
Diego Goya <diego.goya@gmail.com>
2020-05-27 19:41:51 +0200

    third

--- componentes/comunes/Header.js
+++ componentes/comunes/Header.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default ()=>(
 	<div>
 		<Head>
-			<title>Mi curso de Next.js y React</title>
+			<title>La calculadora Natalia</title>
 			<meta name="viewport" content="width=device-width,initial-scale=1.0"/> 
 			 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"/>
 			 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
